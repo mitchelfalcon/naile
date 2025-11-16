c0d0e9a78dcddf7f4eddb13d5b66f707f0825780
Mitchel Falcon <naileafalcon@hotmail.com>
2025-11-16 04:58:09 -0700

    Set up React application with StrictMode

--- main.tsx
+++ main.tsx
@@ -1,1 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/globals.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
 Not Found
